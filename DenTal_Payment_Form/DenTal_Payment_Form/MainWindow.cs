1fb93e003224a6002c28f6d3f21cda4ad3cbac4a
tranChiVi0123 <idiots1230@gmail.com>
2018-09-17 23:00:28 +0700

    Dental_pay

---
+++ DenTal_Payment_Form/DenTal_Payment_Form/MainWindow.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows.Input;
+using System.Windows.Forms;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Gtk.Application.Quit();
+        a.RetVal = true;
+    }
+   
+	protected void OnButtonThoatClicked(object sender, EventArgs e)
+	{
+		Gtk.Application.Quit();
+	}
+
+	protected void OnButtonTinhTienClicked(object sender, EventArgs e)
+	{   
+		string mess = nameEntry.Text.ToString();
+		if(mess==""){
+			MessageBox.Show("Yeu cau nhap ten");
+		}
+		int tongTien = 0;
+		if(checkbuttonCaoVoi.Active){
+			tongTien = tongTien + 100;
+		}
+		if(checkbuttonTayTrang.Active){
+			tongTien = tongTien + 1200;
+		}
+		if(checkbuttonChupHinhRang.Active){
+			tongTien = tongTien + 200;
+		}
+		int soRang = comboboxTramRang.Active;
+		tongTien = tongTien + soRang * 50;
+		entryTong.Text = tongTien.ToString();
+      
+	}
+
+}
---
+++ DenTal_Payment_Form/DenTal_Payment_Form/Properties/Window.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace DenTal_Payment_Form.Properties
+{
+    public partial class Window : Gtk.Window
+    {
+        public Window() :
+                base(Gtk.WindowType.Toplevel)
+        {
+            this.Build();
+        }
+    }
+}
